47bd1a775056ec1bee80e8c40df646b6d4e5171c
Matthes <matthes@leuffen.de>
2019-04-15 23:01:03 +0200

    trying to make work open hotspot

--- script/start_hotspot.sh
+++ script/start_hotspot.sh
@@ -12,7 +12,7 @@ cat >/etc/hostapd/hostapd.conf <<EOF
 interface=$AP_IF
 ssid=raspiki_7Z4F5
 driver=nl80211
-hw_mode=g
+hw_mode=any
 ieee80211n=1
 ieee80211d=1
 channel=$WIFI_CHANNEL
@@ -23,7 +23,7 @@ ignore_broadcast_ssid=0
 wpa=0
 ctrl_interface=/var/run/hostapd
 ctrl_interface_group=0
-#country_code=EU
+country_code=EU
 #wpa_passphrase=
 #wpa_key_mgmt=WPA-PSK
 rsn_pairwise=CCMP
